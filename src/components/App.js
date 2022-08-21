1b10647e7bc34fc1ed0fdc5ac9d8b6d78290c2fb
Eric Gallagher <eric.gallagher@factset.com>
2022-08-21 13:00:28 +0100

    signup done 27.08

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import SignUp from "./SignUp"
+import {Container} from "react-bootstrap";
+import { AuthProvider } from "../contexts/AuthContext";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+
+function App() {
+  
+  return (
+    <>
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center"
+      style={{minHeight: "100vh"}}>
+        <div className="w-100" style={{ maxWidth:"400px"}}> 
+          <SignUp />
+        </div>
+      </Container>  
+    </AuthProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,39 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { auth } from '../firebase';
+
+const AuthContext = React.createContext()
+
+export function useAuth() { //useAuth hook
+    return useContext(AuthContext)
+}
+
+
+export function AuthProvider({children}) {
+    const [currentUser, setCurrentUser] = useState();
+    const [loading, setLoading] = useState(true); //by default we are loading
+    function signup(email, password) {
+        auth.createUserWithEmailAndPassword(email, password) 
+    }
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            setCurrentUser(user)
+            setLoading(false)
+            
+        })
+    return unsubscribe
+    }, [])
+
+
+    const value = {
+        currentUser,
+        signup
+    }
+
+
+  return (
+    <AuthContext.Provider value={value}>
+        {!loading && children}
+    </AuthContext.Provider>    
+  )
+}
