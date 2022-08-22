be8ef483cc41d942620e3351b2f26b1b8e750552
Eric Gallagher <eric.gallagher@factset.com>
2022-08-22 18:01:34 +0100

    db working babye

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,11 +1,11 @@
 import Navbar from "../components/Navbar"
-
+import PApp from "../pdf/src/PApp"
 export default function Books() {
   return (
   <>
   <Navbar />
   <h1>Books</h1>
-  
+  <PApp />
   
   
   </>
