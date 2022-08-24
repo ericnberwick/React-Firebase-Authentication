a3468360289312996cbfce6e1fd21df45a86de0c
Eric Gallagher <eric.gallagher@factset.com>
2022-08-24 09:49:12 +0100

    ideas

---
+++ src/ideas.js
@@ -0,0 +1,12 @@
+{/* 
+
+Have payment system before login and then only signed up users can see premium notes
+
+
+Have lock icons on premium notes that won't display to non premium members.
+
+Have different color note icon depending on section
+
+
+
+*/}
