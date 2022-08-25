b12a00e06f0f3dc88a975722c498e1168fcbd01e
Eric Gallagher <eric.gallagher@factset.com>
2022-08-25 22:44:19 +0100

    landed

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPencil} from '@fortawesome/free-solid-svg-icons';
 import PlantBackground from "../pics/back1.png";
 import { useContext } from "react";
 import FbNavbar from "../FbNavbar";
@@ -9,13 +11,14 @@ import FbNavbar from "../FbNavbar";
  * @returns
  */
 function Landing() {
+
   return (
     <>
     <FbNavbar />
       <div className="hero-container">
         <img className="background-image" src={PlantBackground}></img>
-        <h1 style={{ color: "white", marginTop: 10 }}>Welcome to notEd.</h1>
-        <p style={{ color: "white" }}>
+        <h1 style={{ color: "white", marginTop: 10 }}>Welcome to not<span className="ed">Ed</span>.</h1>
+        <p className="para">
           Explore, Learn and Create from our library of professional note library
         </p>
         
