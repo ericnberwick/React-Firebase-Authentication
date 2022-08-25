8f7820f62cb6050b03fd2947148bc5ccdee536e2
Eric Gallagher <eric.gallagher@factset.com>
2022-08-25 18:11:04 +0100

    footer in the correct position

--- src/components/App.js
+++ src/components/App.js
@@ -28,7 +28,6 @@ function App() {
   return (
     <Container
       className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
     >
       <div className="w-100" style={{ maxWidth: "400px" }}>
         <Router>
