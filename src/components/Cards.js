a59fff9061e33a4b563c8d281fcd8f4b682b6b09
Eric Gallagher <eric.gallagher@factset.com>
2022-08-26 19:21:26 +0100

    cards added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -23,71 +23,83 @@ function Cards({section}) {
               src={Carrots}
               text="HTML for Beginners"
               path="/html1"
+              file="1___HTML_Fundamentals.pdf"
             />
           
             <CardItem
               src={Tomato}
               text="CSS for Beginners"
-              path="/art3"
+              path="/comp2"
+              file="2___CSS_Fundamentals.pdf"
             />
             <CardItem
               src={Tomato}
               text="Developing Websites Locally"
-              path="/art3"
+              path="/comp3"
+              file="3___Developing_Website_Locally.pdf"
             />
           </ul>
           <ul className="cards__items">
             <CardItem
               src={Carrots}
               text="Improved Styling CSS"
-              path="/html1"
+              path="/comp4"
+              file="4___Improved_Styling_CSS.pdf"
             />
           
             <CardItem
               src={Tomato}
               text="Fundamentals of Web Design"
-              path="/art3"
+              path="/comp5"
+              file="5___Fundamentals_of_Web_Design.pdf"
             />
             <CardItem
               src={Tomato}
               text="Making a Website Responsive"
-              path="/art3"
+              path="/comp6"
+              file="6___Making_a_Website_Responsive.pdf"
             />
           </ul>
           <ul className="cards__items">
             <CardItem
               src={Carrots}
               text="JavaScipt for Beginners"
-              path="/html1"
+              path="/comp7"
+              file="7___JavaScript.pdf"
             />
           
             <CardItem
               src={Tomato}
               text="JavaScript for Intermediate"
-              path="/art3"
+              path="/comp8"
+              file="10___More_JavaScipt.pdf"
             />
             <CardItem
               src={Tomato}
               text="JavaScript Testing"
-              path="/art3"
+              path="/comp9"
+              file="11___JavaScript_Testing.pdf"
             />
           </ul>
           <ul className="cards__items">
             <CardItem
               src={Carrots}
               text="Asyn JavaScript and HTTP Requests"
-              path="/html1"
+              path="/comp10"
+              file="12___Async_JavaScript_and_HTTP_Requests.pdf"
             />
           
             <CardItem
               src={Tomato}
               text="React"
-              path="/art3"
+              path="/comp11"
+              file="13___React.pdf"
             />
             <CardItem
               src={Tomato}
-              text="Redux"
-              path="/art3"
+              text="Git"
+              path="/comp12"
+              file="Git_Notes.pdf"
             />
           </ul>
         </div>
