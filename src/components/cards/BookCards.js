0f4621ea28a6ede2aa9c2ddde062d4823cb7a367
Eric Gallagher <eric.gallagher@factset.com>
2022-08-27 17:57:22 +0100

    5 rows of books

--- src/components/cards/BookCards.js
+++ src/components/cards/BookCards.js
@@ -28,12 +28,14 @@ function Cards({section}) {
               text="7 Habits of Highly Effective People"
               path="/art3"
               file="Limitless___Jim_Kwik.pdf"
+              book ="https://images-na.ssl-images-amazon.com/images/I/51hV5vGr4AL.jpg"
             />
             <BookCardItem
               src={Tomato}
               text="Think and Grow Rich"
               path="/art3"
               file="Limitless___Jim_Kwik.pdf"
+              book ="https://images-na.ssl-images-amazon.com/images/I/71ZA+vKnFRL.jpg"
             />
           </ul>
           <ul className="cards__items">
@@ -42,6 +44,7 @@ function Cards({section}) {
               text="The Subtle Art of Not Giving a F***"
               path="/html1"
               file="Limitless___Jim_Kwik.pdf"
+              book="https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L.jpg"
             />
           
             <BookCardItem
@@ -49,12 +52,62 @@ function Cards({section}) {
               text="Atomic Habits"
               path="/art3"
               file="Limitless___Jim_Kwik.pdf"
+              book="https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg"
             />
             <BookCardItem
               src={Tomato}
               text="The Infinite Game"
               path="/art3"
               file="Limitless___Jim_Kwik.pdf"
+              book = "https://images-na.ssl-images-amazon.com/images/I/51YNDYmRqBL.jpg"
+            />
+          </ul>
+          <ul className="cards__items">
+            <BookCardItem
+              src={Carrots}
+              text="How To Win Friends & Influence People"
+              path="/html1"
+              file="Limitless___Jim_Kwik.pdf"
+              book="https://m.media-amazon.com/images/I/41FYFMn7lZL._AC_SY780_.jpg"
+            />
+          
+            <BookCardItem
+              src={Tomato}
+              text="Deep Work"
+              path="/art3"
+              file="Limitless___Jim_Kwik.pdf"
+              book="https://images-na.ssl-images-amazon.com/images/I/71g8BUSqbpL.jpg"
+            />
+            <BookCardItem
+              src={Tomato}
+              text="The Worlds Fittest Book"
+              path="/art3"
+              file="Limitless___Jim_Kwik.pdf"
+              book = "https://images-na.ssl-images-amazon.com/images/I/71JtF90V+DL.jpg"
+            />
+          </ul>
+          <ul className="cards__items">
+            <BookCardItem
+              src={Carrots}
+              text="Build a Bulletproof Body For Extreme Adventure In 365 Days"
+              path="/html1"
+              file="Limitless___Jim_Kwik.pdf"
+              book="https://cdn.waterstones.com/bookjackets/large/9780/0084/9780008487034.jpg"
+            />
+          
+            <BookCardItem
+              src={Tomato}
+              text="Thinking Fast and Slow"
+              path="/art3"
+              file="Limitless___Jim_Kwik.pdf"
+              book="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1317793965l/11468377.jpg"
+            />
+            <BookCardItem
+              src={Tomato}
+              text="Rich Dad Poor Dad"
+              path="/art3"
+              file="Limitless___Jim_Kwik.pdf"
+              book = "https://images-na.ssl-images-amazon.com/images/I/81bsw6fnUiL.jpg"
             />
           </ul>
           
