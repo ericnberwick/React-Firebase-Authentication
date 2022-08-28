ee5c9b8d1d851ea3b0928dc77e6112c761d59b6e
Eric Gallagher <eric.gallagher@factset.com>
2022-08-28 15:52:39 +0100

    book 1 page has full functionality

--- src/pages/books/book1.js
+++ src/pages/books/book1.js
@@ -1,8 +1,18 @@
 import Comp from "./Comp"
+import { useAuth } from "../../contexts/AuthContext.js";
 export default function Book1() {
+  var loggedIn;
+  const { currentUser, logout } = useAuth()
+  if (currentUser != null){
+    loggedIn = true;
+  } else {
+    loggedIn = false;
+  }
   return (
   <>
-    <Comp title="Limitless" Afile="Limitless___Jim_Kwik.pdf" cover="https://images-na.ssl-images-amazon.com/images/I/71NApfLNYjL.jpg"/>
+    {loggedIn && <Comp title="Limitless" Afile="Limitless___Jim_Kwik.pdf" cover="https://images-na.ssl-images-amazon.com/images/I/71NApfLNYjL.jpg"/>}
+    {!loggedIn && <Comp title="Limitless" Afile="upgradeForFullAccess.pdf" cover="https://images-na.ssl-images-amazon.com/images/I/71NApfLNYjL.jpg"/>}
+    
   </>
   )
 }
