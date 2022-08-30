2cca385f60246c0eecce1229f7bf698681b415e7
Eric Gallagher <eric.gallagher@factset.com>
2022-08-30 17:24:54 +0100

    Rnavbar stuff

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -4,6 +4,7 @@ import { loadStripe } from "@stripe/stripe-js";
 import CardIcon from "../images/credit-card.svg";
 import ProductImage from "../images/checkout.jpg";
 import FbNavbar from "../FbNavbar";
+import RNavbar from "./RNavbar/RNavbar";
 import "./styles.css";
 
 
